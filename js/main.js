cbc66b916dcdcb6b7e2a4f720c52e3655fb78d52
SergWulf <36727388+SergWulf@users.noreply.github.com>
2020-09-25 10:50:57 +0300

    Добавил скрытие блоков в карточке, если нету исходных данных

--- js/main.js
+++ js/main.js
@@ -134,8 +134,8 @@ const pinTemplate = document.querySelector('#pin').content.querySelector('.map__
 const renderPin = function (realEstatePin) {
   const pinElement = pinTemplate.cloneNode(true);
   const pinPointerCoordinateX = Number(realEstatePin['location']['x']) - COORDINATE_PIN_X;
-  const pinPointerCoordianteY = Number(realEstatePin['location']['y']) - COORDINATE_PIN_Y;
-  pinElement.style = 'left: ' + pinPointerCoordinateX + 'px; top: ' + pinPointerCoordianteY + 'px;';
+  const pinPointerCoordinateY = Number(realEstatePin['location']['y']) - COORDINATE_PIN_Y;
+  pinElement.style = 'left: ' + pinPointerCoordinateX + 'px; top: ' + pinPointerCoordinateY + 'px;';
   pinElement.querySelector('img').src = realEstatePin['author']['avatar'];
   pinElement.querySelector('img').alt = realEstatePin['offer']['title'];
   return pinElement;
@@ -157,37 +157,85 @@ blockPins.appendChild(renderPins(realEstates));
 // Создаем шаблон для отображения карточки объекта недвижимости
 const cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 
-// Функция отображения карточки
+// Функция отображения карточки, если данных для заполнения блока не хватает, то блок скрывается
 const renderCard = function (realEstateCard) {
   const cardElement = cardTemplate.cloneNode(true);
-  cardElement.querySelector('.popup__title').textContent = realEstateCard['offer']['title'];
-  cardElement.querySelector('.popup__text--address').textContent = realEstateCard['offer']['address'];
-  cardElement.querySelector('.popup__text--price').innerHTML = realEstateCard['offer']['price'] + '&#x20bd;' + '<span>/ночь</span>';
-  cardElement.querySelector('.popup__type').textContent = TYPE_RESIDENCE[realEstateCard['offer']['type']];
-  cardElement.querySelector('.popup__text--capacity').textContent = realEstateCard['offer']['rooms'] + ' комнаты для ' + realEstateCard['offer']['guests'] + ' гостей';
-  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + realEstateCard['offer']['checkin'] + ', выездо до ' + realEstateCard['offer']['checkout'];
+
+  if (realEstateCard['offer']['title']) {
+    cardElement.querySelector('.popup__title').textContent = realEstateCard['offer']['title'];
+  } else {
+    cardElement.querySelector('.popup__title').classList.add('visually-hidden');
+  }
+  if (realEstateCard['offer']['address']) {
+    cardElement.querySelector('.popup__text--address').textContent = realEstateCard['offer']['address'];
+  } else {
+    cardElement.querySelector('.popup__text--address').classList.add('visually-hidden');
+  }
+  if (realEstateCard['offer']['price']) {
+    cardElement.querySelector('.popup__text--price').innerHTML = realEstateCard['offer']['price'] + '&#x20bd;' + '<span>/ночь</span>';
+  } else {
+    cardElement.querySelector('.popup__text--price').classList.add('visually-hidden');
+  }
+  if (realEstateCard['offer']['type']) {
+    cardElement.querySelector('.popup__type').textContent = TYPE_RESIDENCE[realEstateCard['offer']['type']];
+  } else {
+    cardElement.querySelector('.popup__type').classList.add('visually-hidden');
+  }
+  if (realEstateCard['offer']['rooms']) {
+    cardElement.querySelector('.popup__text--capacity').textContent = realEstateCard['offer']['rooms'] + ' комнаты для ' + realEstateCard['offer']['guests'] + ' гостей';
+  } else {
+    cardElement.querySelector('.popup__text--capacity').classList.add('visually-hidden');
+  }
+  if ((realEstateCard['offer']['checkin']) && (realEstateCard['offer']['checkout'])) {
+    cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + realEstateCard['offer']['checkin'] + ', выездо до ' + realEstateCard['offer']['checkout'];
+  } else {
+    cardElement.querySelector('.popup__text--time').classList.add('visually-hidden');
+  }
+
   // В разметке находим блок предоставления услуг
   const popupFeatures = cardElement.querySelector('.popup__features');
   // Удаляем все виды услуг из разметки
   while (popupFeatures.firstChild) {
     popupFeatures.removeChild(popupFeatures.firstChild);
   }
-  // Добавляем нужные услуги в разметку
-  for (let i = 0; i < realEstateCard['offer']['features'].length; i++) {
-    const elementFeature = document.createElement('li');
-    elementFeature.className = 'popup__feature popup__feature--' + realEstateCard['offer']['features'][i];
-    popupFeatures.appendChild(elementFeature);
+  if (realEstateCard['offer']['features'].length !== 0) {
+    // Добавляем нужные услуги в разметку
+    for (let i = 0; i < realEstateCard['offer']['features'].length; i++) {
+      const elementFeature = document.createElement('li');
+      elementFeature.className = 'popup__feature popup__feature--' + realEstateCard['offer']['features'][i];
+      popupFeatures.appendChild(elementFeature);
+    }
+  } else {
+    popupFeatures.classList.add('visually-hidden');
   }
-  cardElement.querySelector('.popup__description').textContent = realEstateCard['offer']['description'];
+
+  if (realEstateCard['offer']['description']) {
+    cardElement.querySelector('.popup__description').textContent = realEstateCard['offer']['description'];
+  } else {
+    cardElement.querySelector('.popup__description').classList.add('visually-hidden');
+  }
   // Добавляем фотографии в карточку объекта недвижимости
   const popupPhotos = cardElement.querySelector('.popup__photos');
-  for (let i = 0; i < realEstateCard['offer']['photos'].length; i++) {
-    const popupPhoto = popupPhotos.querySelector('img').cloneNode(true);
-    popupPhoto.src = realEstateCard['offer']['photos'][i];
-    popupPhotos.appendChild(popupPhoto);
+
+  if (realEstateCard['offer']['photos'].length > 0) {
+    for (let i = 0; i < realEstateCard['offer']['photos'].length; i++) {
+      const popupPhoto = popupPhotos.querySelector('img').cloneNode(true);
+      popupPhoto.src = realEstateCard['offer']['photos'][i];
+      popupPhotos.appendChild(popupPhoto);
+    }
+    // Удаляем шаблонную фотокарточку.
+    popupPhotos.children[0].remove();
+  } else {
+    popupPhotos.classList.add('visually-hidden');
   }
-  popupPhotos.removeChild(popupPhotos.children[0]);
-  cardElement.querySelector('.popup__avatar').src = realEstateCard['author']['avatar'];
+
+
+  if (realEstateCard['author']['avatar']) {
+    cardElement.querySelector('.popup__avatar').src = realEstateCard['author']['avatar'];
+  } else {
+    cardElement.querySelector('.popup__avatar').classList.add('visually-hidden');
+  }
+
   return cardElement;
 };
 

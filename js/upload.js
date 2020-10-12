41bc848799a8a822fdc2570822ed7a00c49c9b4e
Sergey Volkov <serg.wulf.s@gmail.com>
2020-10-12 06:41:24 +0300

    Создание модуля отправки данных. Часть1

---
+++ js/upload.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function () {
+  const URL = 'https://21.javascript.pages.academy/keksobooking';
+
+  const uploadData = function (data, onSuccess) {
+    const xhr = new XMLHttpRequest();
+  };
+
+})();
